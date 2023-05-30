44806d5adc96d16a1cb9abbe269d1d2adaf4ead4
Tim Schaefer <ts+code@rcmd.org>
2023-05-30 23:44:29 +0200

    NEW: add links

--- pkg/freesurfer/read_fs_mesh.go
+++ pkg/freesurfer/read_fs_mesh.go
@@ -2,6 +2,9 @@ package freesurfer
 
 import "fmt"
 
+// https://pkg.go.dev/github.com/oschwald/maxminddb-golang#example-Reader.Lookup-Interface
+// https://pkg.go.dev/encoding/binary#example-Read-Multi
+
 func main() {
 	fmt.Println("hello world")
 }
