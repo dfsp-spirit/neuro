513df4fe2d90bea06d14c65f1742978b75cde4e2
Tim Schäfer <ts+code@rcmd.org>
2023-06-07 21:34:39 +0200

    NEW: add unit test for ReadFsCurv

---
+++ read_fs_curv_test.go
@@ -0,0 +1,38 @@
+package neuro
+
+// https://pkg.go.dev/testing
+
+import (
+	"testing"
+)
+
+func TestReadFsCurv(t *testing.T){
+	var curvFile string = "testdata/lh.thickness"
+
+	// Read the curv file
+	pvdata, _ := ReadFsCurv(curvFile)
+
+	got := len(pvdata)
+	want := 149244
+
+	if got != want {
+		t.Errorf("got data for %d vertices in curv file, wanted %d", got, want)
+	}
+
+	// Compute some stats on the data and check them
+	gotMax, _ := max(pvdata)
+	if gotMax < 2.0 || gotMax > 5.0 {
+		t.Errorf("got max value %f in curv file, wanted between 2.0 and 5.0", gotMax)
+	}
+
+	gotMin, _ := min(pvdata)
+	if gotMin < 0.0 || gotMin > 2.0 {
+		t.Errorf("got min value %f in curv file, wanted between 0.0 and 2.0", gotMin)
+	}
+
+	gotMean, _ := mean(pvdata)
+	if gotMean < 1.0 || gotMean > 3.0 {
+		t.Errorf("got mean value %f in curv file, wanted between 1.0 and 3.0", gotMean)
+	}
+
+}
